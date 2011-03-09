3c11d4e68779a7f31c05ad03c3649af5bcbc3566
Lindsay Evans <linz@linz.id.au>
2011-03-09 13:57:27 +1100

    Adding 204 No Response method; tidying

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
 
-// Where to log to
+// Type of resonse to send back
+// - 'no response': returns a '204 No Content' response & no body (0B)
+// - 'gif': returns a non-cachable 1x1 transparent GIF (43B)
+$response_type = 'no response';
+
+// Default file to log to
 $log_file = 'noscript.log';
+
+// Format of the log file (same as Apache Combined)
+$log_format = "%s - - %s \"%s\" %d %d \"%s\" \"%s\"\n";
 
 // Stick in a domain specific log file if supplied with a domain argument
 if(array_key_exists('domain', $_GET)){
 	$log_file = 'noscript.' . $_GET['domain'] . '.log';
 }
-
-// Format of the log file (same as Apache Combined)
-$log_format = "%s - - %s \"%s\" %d %d \"%s\" \"%s\"\n";
 
 // Open log file & lock
 $fp = fopen($log_file, 'a');
@@ -34,12 +39,17 @@ if($fp && flock($fp, LOCK_EX | LOCK_NB)){
 // Close log file
 fclose($fp);
 
-// Output 1px transparent gif
-header('Content-type: image/gif');
-header('Expires: Fri, 24 Jan 2042 12:34:56 GMT');
-header('Cache-Control: no-cache');
-header('Cache-Control: must-revalidate');
-printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",71,73,70,56,57,97,1,0,1,0,128,255,0,192,192,192,0,0,0,33,249,4,1,0,0,0,0,44,0,0,0,0,1,0,1,0,0,2,2,68,1,0,59);
+if($response_type == 'no response'){
+	// Send '204 No Content' response
+	header('HTTP/1.0 204 No Content');
+}else{
+	// Output 1px transparent gif
+	header('Content-type: image/gif');
+	header('Expires: Fri, 24 Jan 2042 12:34:56 GMT');
+	header('Cache-Control: no-cache');
+	header('Cache-Control: must-revalidate');
+	printf ("%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",71,73,70,56,57,97,1,0,1,0,128,255,0,192,192,192,0,0,0,33,249,4,1,0,0,0,0,44,0,0,0,0,1,0,1,0,0,2,2,68,1,0,59);
+}
 
 exit(0);
 
